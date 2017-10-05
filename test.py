3570b5af77ccbb1560fe506bf479acff420a8b4b
Shuo Han <shan50@jhu.edu>
2017-10-05 15:34:34 -0400

    added colors

--- test.py
+++ test.py
@@ -7,16 +7,23 @@ import nibabel as nib
 import copy
 import scipy.misc as smp
 from time import time
+import sys
 
 image_path = 'image.nii'
 labels_path = 'labels.nii'
+colors_path = '/home/shuo/projects/test_label_colors/colors.npy'
 
 sliceid = 100
-alpha = 0.5
+alpha = 0.1
 
 start = time()
 image = np.swapaxes(nib.load(image_path).get_data(), 0, 1)
 labels = np.swapaxes(nib.load(labels_path).get_data(), 0, 1)
+labels = np.digitize(labels, np.unique(labels), right=True)
+print(labels.dtype)
+
+colors = np.load(colors_path)
+
 print('load', time() - start)
 
 mask = labels != 0
@@ -29,11 +36,23 @@ rest = image * inverse_mask
 print('numpy', time() - start)
 
 start = time()
-overlay_image_pil = smp.toimage(overlay_image[:, :, sliceid])
-overlay_labels_pil = smp.toimage(overlay_labels[:, :, sliceid])
+image_slice = overlay_image[:, :, sliceid]
+stacked_overlay_image = np.dstack([image_slice, image_slice, image_slice])
+overlay_image_pil = smp.toimage(stacked_overlay_image)
+
+labels_slice = overlay_labels[:, :, sliceid]
+stacked_labels = np.dstack([colors[labels_slice, 0], colors[labels_slice, 1], 
+                            colors[labels_slice, 2]]) 
+overlay_labels_pil = smp.toimage(stacked_labels)
+
 overlay_pil = Image.blend(overlay_image_pil, overlay_labels_pil, alpha)
-rest_pil = smp.toimage(rest[:, :, sliceid])
+overlay_pil.show()
+
+rest_slice = rest[:, :, sliceid]
+stacked_rest = np.dstack([rest_slice, rest_slice, rest_slice])
+rest_pil = smp.toimage(stacked_rest)
 result = ImageChops.add(overlay_pil, rest_pil)
+
 print('pil', time() - start)
 
 result.show()
