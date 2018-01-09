82ae67ed47a0e6b1ef70a782d4715a02b33fa62c
Shuo Han <shan50@jhu.edu>
2018-01-08 19:03:52 -0500

    Add saving option for GUI

--- gui.py
+++ gui.py
@@ -4,6 +4,7 @@
 
 """
 
+import os
 from PyQt5.QtCore import QDir, Qt, QTimer
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtWidgets import (QApplication, QCheckBox, QFileDialog, QGridLayout,
@@ -18,6 +19,8 @@ class ImageWindow(QLabel):
     """ Provide a window containing the overlay picture
 
     Args:
+        image_path (file, *.nii or *.nii.gz): The path to the image to show
+        image_path (file, *.nii or *.nii.gz): The path to the label image on top
         slice_idx (int): The index of the current showing slice
         orient (str): The orientation {'axial', 'coronal', 'sagittal'}
         alpha (float): The alpha value in [0, 1] of the label image
@@ -30,7 +33,7 @@ class ImageWindow(QLabel):
 
     def __init__(self, image_path, label_image_path, colors,
                  orient='axial', initial_slice_idx=None, initial_alpha=0.5,
-                 need_to_convert_colors=True):
+                 need_to_convert_colors=True, output_dir=None):
         """
         Args:
             image_path (str): The path to the image to display
@@ -49,6 +52,10 @@ class ImageWindow(QLabel):
                 convert 2, 5, 10 to 0, 1, 2), use this option to convert the
                 colors array so that (2, 5, 10) rows of the new array has the
                 (0, 1, 2) rows of the original colors
+            output_dir (str): If not None, pressing enter will not save the 
+                image; otherwise, the image will be save as
+                {output_dir}/{image_name}_orient_{orient}_slice_{slice_idx}.png
+                `image_name` will be the same with the input image
 
         """
         super(ImageWindow, self).__init__()
@@ -56,6 +63,9 @@ class ImageWindow(QLabel):
                                              colors, need_to_convert_colors)
         # setting orient requires slice_idx but setting slice_idx also requires
         # the orient
+        self.image_path = image_path
+        self.label_image_path = label_image_path
+        self.output_dir = output_dir
         self._slice_idx = None
         self.orient = orient
         self.slice_idx = initial_slice_idx
@@ -114,10 +124,12 @@ class ImageWindow(QLabel):
         """Update the image within the window to show
 
         """
-        image = self._image_renderer.get_slice(self.orient, self.slice_idx,
-                                               self.alpha, self._width_zoom,
-                                               self._height_zoom)
-        self.setPixmap(QPixmap.fromImage(ImageQt(image)))
+        self.image_slice = self._image_renderer.get_slice(self.orient,
+                                                          self.slice_idx,
+                                                          self.alpha,
+                                                          self._width_zoom,
+                                                          self._height_zoom)
+        self.setPixmap(QPixmap.fromImage(ImageQt(self.image_slice)))
 
     def keyPressEvent(self, e):
         """The events when pressing a key
@@ -145,6 +157,19 @@ class ImageWindow(QLabel):
                 self.orient = 'sagittal'
             elif self.orient == 'sagittal':
                 self.orient = 'axial'
+        elif e.key() == Qt.Key_Return:
+            if self.output_dir is not None:
+                if not os.path.isdir(self.output_dir):
+                    os.makedirs(self.output_dir)
+                image_name = os.path.basename(self.image_path).split('.')[0]
+                slice_str = '%03d' % self.slice_idx
+                output_filename = '_'.join([image_name, 'orient', self.orient,
+                                            'slice', slice_str]) + '.png'
+                output_filename = os.path.join(self.output_dir, output_filename)
+                self.image_slice.save(output_filename, 'PNG')
+                print('Save to', output_filename)
+            else:
+                print('Output file is not set, cannot save image')
         self._update()
 
     def resizeEvent(self, e):
