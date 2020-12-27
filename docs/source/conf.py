fb37420b6b8b8df2439da5af0895db3defbf277a
Shuo Han <shan50@jhu.edu>
2020-12-27 00:49:08 -0500

    Update sphinx conf for mock imports

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -61,6 +61,6 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
 
-autodoc_mock_imports = ['numpy', 'scipy']
+autodoc_mock_imports = ['numpy', 'scipy', 'PIL', 'improc3d']
 # add_module_names = False
 doctest_test_doctest_blocks = ''
