1ac03ebc0d6e5e73c043f90e2e2c0b32ef26eb33
Shuo Han <shan50@jhu.edu>
2020-12-27 16:42:29 -0500

    Concatenate slices into a single file

--- tests/test_convert.sh
+++ tests/test_convert.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ../scripts/convert_nii -i image.nii.gz -v axial -o results_convert/image_axial
-../scripts/convert_nii -i image.nii.gz -v coronal -o results_convert/image_coronal -q -A
-../scripts/convert_nii -i image.nii.gz -l label.nii.gz -v coronal -o results_convert/pair_coronal -q -a 0.5
+../scripts/convert_nii -i image.nii.gz -v coronal -o results_convert/image_coronal -q -A -C -R -P 0 0 255 255
+../scripts/convert_nii -i image.nii.gz -l label.nii.gz -v coronal -o results_convert/pair_coronal -q -a 0.5 -C
 ../scripts/convert_nii -i image.nii.gz -l label.nii.gz -v coronal -o results_convert/pair_coronal_reindex -q -a 0.8 -r -S -E 1 -s
-../scripts/convert_nii -i image.nii.gz -l label.nii.gz -c colormap.txt -v sagittal -o results_convert/edge_sagittal_colormap -q -a 1 -e -d 3 -s
+../scripts/convert_nii -i image.nii.gz -l label.nii.gz -c colormap.txt -v sagittal -o results_convert/edge_sagittal_colormap -q -a 1 -e -d 3 -s -C -S
