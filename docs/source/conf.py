64d442ad931da21f6d511c1d73dfef1da6dd6d95
Shuo Han <shan50@jhu.edu>
2020-12-28 13:29:21 -0500

    Force to use python3 to run scripts

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Shuo Han'
 author = 'Shuo Han'
 
 # The full version, including alpha/beta/rc tags
-release = '3.1.1'
+release = '3.1.2'
 
 
 # -- General configuration ---------------------------------------------------
