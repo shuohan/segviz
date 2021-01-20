7cb9833b309fbbc6bd2107a5f209f8fcd6e64610
Shuo Han <shan50@jhu.edu>
2021-01-20 18:29:33 -0500

    Update doc config

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,15 +30,7 @@ release = '3.1.2'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-]
 
-# Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
 extensions = [
         'sphinx.ext.intersphinx',
         'sphinx.ext.napoleon',
@@ -48,6 +40,13 @@ extensions = [
         'sphinx.ext.doctest',
         'sphinx.ext.todo'
 ]
+
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
 
 # -- Options for HTML output -------------------------------------------------
 
